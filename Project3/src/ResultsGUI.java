3c0fd6c7dee3c98739d6cc033f4a9a19c7a36cfd
Brian Kenji Aguilar <anjacular@gmail.com>
2018-04-27 13:57:11 -0700

    Project 3

---
+++ Project3/src/ResultsGUI.java
@@ -0,0 +1,84 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.StringTokenizer;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class ResultsGUI extends JFrame {
+	
+	//Instance variables for a ResultsGUI
+	private int length,width,height;
+	private JTextArea sorted;
+	
+	/*
+	 * Default Constructor for the ResultsGUI. Sets up the main components
+	 * to the GUI such as the title, size, location etc. of the GUI
+	 * @param length the length that the user wants
+	 * @param width the width that the user wants
+	 * @param height the height that the user wants
+	 */
+	public ResultsGUI(int length, int width, int height) {
+		this.length=length;
+		this.width=width;
+		this.height=height;
+		setTitle("Results for Boxes with L:"+length+" W:"+width+" H:"+height);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(600,400);
+		setLocationRelativeTo(null);
+		setComponenets();
+		searchResults();
+		setVisible(true);
+	}
+	
+	/*
+	 * Sets up the components needed for the GUI
+	 */
+	private void setComponenets() {
+		/* 
+		 * Sets the layout of the GUI like a grid. This grid layout
+		 * contains one row and one column 
+		 */
+		setLayout(new GridLayout(1,1));
+		
+		/*
+		 * Displays the list of Boxes that meet the 
+		 * following criteria in a text area with some decorations 
+		 * such as a different background color and text style
+		 */
+		sorted = new JTextArea("Results:\n");
+		sorted.setBackground(new Color(0,230,0));
+		sorted.setFont(new Font(Font.DIALOG, Font.BOLD, 13));
+		sorted.setEditable(false);
+		sorted.setBorder(BorderFactory.createLineBorder(Color.black));
+	
+		//Adds the TextArea to the GUI 
+		add(sorted);
+	
+	}//setComponents
+	
+	/*
+	 * A helper function that displays all the boxes
+	 * that meets the criteria for the user
+	 */
+	private void searchResults() {
+		
+		//Linear search for a linked list
+		BoxNode sortedSearch= BoxGUI.sortedList.first.next;
+
+		while(sortedSearch != null) {
+			
+			if((sortedSearch.data).getLength()>=length && (sortedSearch.data).getWidth()>=width && (sortedSearch.data).getHeight()>=height) {
+				sorted.append(sortedSearch.data.toString()+"\n");
+			}
+
+			
+			sortedSearch = sortedSearch.next;
+
+		}
+		
+	}//serachResults
+
+}//ResultsGUI
